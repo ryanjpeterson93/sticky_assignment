af8d9fc68ceb28454b21d3f210a94299438c7dce
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-02-18 15:38:07 -0700

    modified existing files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'notes/index'
-  get 'notes/show'
-  get 'notes/new'
+  root 'notes#index'
+
+  resources :notes
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
