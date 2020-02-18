f21ccb74caca94d434ffb46ddcdacc872f582da5
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-02-18 15:47:17 -0700

    added New Note ability

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -10,4 +10,20 @@ class NotesController < ApplicationController
   def new
     @note = Note.new
   end
+
+  def create
+    @note = Note.new(notes_params)
+
+   if @note.save
+     redirect_to notes_path
+   else
+     render :new
+   end
+  end
+
+  private
+  def notes_params
+    params.require(:note).permit(:title, :author, :body)
+  end
+
 end
