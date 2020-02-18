059e18d884f90c79cfa7997dbd75184d0f6eea59
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-02-18 16:14:50 -0700

    Added update functionality

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,6 +21,21 @@ class NotesController < ApplicationController
    end
   end
 
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+  
+    if @note.update(notes_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+
   private
   def notes_params
     params.require(:note).permit(:title, :author, :body)
